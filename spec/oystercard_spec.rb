e87b859ac0c7531a406c857dc078abf3af823562
michkles <michkles@gmail.com>
2017-04-02 20:48:09 +0100

    guard condition and top_up functionality to oystercard added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,19 @@ describe Oystercard do
   it 'has a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  describe "#top_up" do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance ' do
+      expect { subject.top_up(20).to change { subject.balance }.by 20 }
+    end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      max_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(max_balance)
+      expect { subject.top_up 1}.to raise_error "Maximum balance of #{max_balance} is exceeded"
+    end
+  end
 end
